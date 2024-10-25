9e343318ef2eb4083836c383670fd3e7bfa64f79
qxy <qxy@qxy-Ubuntu20.04>
2024-10-25 15:45:37 +0800

    test gazebo obs

--- tensorrt/onnx_to_tensorrt.py
+++ tensorrt/onnx_to_tensorrt.py
@@ -69,11 +69,20 @@ def main():
     # Contexts are used to perform inference.
     context = engine.create_execution_context()
 
+    inp = np.zeros((1,1170))
+    inp[0,-39:] = np.array([-0.01817463,  0.00316524, -0.99982983, -0.001551,    0.00625625, -0.02259382,
+  0.00605177,  0.03229075, -0.01595549, -0.00646622, -0.01030582, -0.03389338,
+  0.00265833, -0.0066327,  -0.02148847, -0.00350176, -0.04536879,  0.08309361,
+  0.00468482, -0.06002165,  0.10744572, -0.0034748,  -0.05103638,  0.08126458,
+  0.00258044, -0.03308586,  0.05241467,  0.,          0.       ,   0.,
+  0.    ,      0.   ,       0. ,         0.  ,        0.,          0.,
+  0.   ,       0.    ,      0.        ]
+)
     
-    np.copyto(inputs[0].host, np.zeros((1,1170)))
+    np.copyto(inputs[0].host, inp)
 
     t = []
-    for i in range(100):
+    for i in range(10):
         t1 = time.time()
         trt_outputs = common.do_inference(
             context,
--- tensorrt/pytorch_to_onnx.py
+++ tensorrt/pytorch_to_onnx.py
@@ -20,7 +20,7 @@ OmegaConf.register_new_resolver("round_down", math.floor)
 def main(cfg: OmegaConf):
     OmegaConf.resolve(cfg)
     model = hydra.utils.instantiate(cfg.model)
-    data = torch.load("../state_8000.pt", weights_only=True)
+    data = torch.load("/home/qxy/dppo/state_8000.pt", weights_only=True)
 
     model.load_state_dict(data["model"])
     print("Load Model successfully!")
@@ -28,6 +28,14 @@ def main(cfg: OmegaConf):
     model.eval()
     # torch.manual_seed(0)
     torch_input = torch.zeros(1, 1170).to("cuda:0")
+    torch_input[0,-39:] = torch.tensor([-0.01817463,  0.00316524, -0.99982983, -0.001551,    0.00625625, -0.02259382,
+  0.00605177,  0.03229075, -0.01595549, -0.00646622, -0.01030582, -0.03389338,
+  0.00265833, -0.0066327,  -0.02148847, -0.00350176, -0.04536879,  0.08309361,
+  0.00468482, -0.06002165,  0.10744572, -0.0034748,  -0.05103638,  0.08126458,
+  0.00258044, -0.03308586,  0.05241467,  0.,          0.       ,   0.,
+  0.    ,      0.   ,       0. ,         0.  ,        0.,          0.,
+  0.   ,       0.    ,      0.        ]
+)
     # input = {"state": torch.randn(1,1,23).to("cuda:0")}
     # for i in range(10):
     #     t1 = time.time()
@@ -53,6 +61,15 @@ def main(cfg: OmegaConf):
 
     import onnxruntime 
     ort_session = onnxruntime.InferenceSession("trotting_export.onnx") 
+    inputs = np.zeros((1,1170))
+    inputs[0,-39:] = np.array([-0.01817463,  0.00316524, -0.99982983, -0.001551,    0.00625625, -0.02259382,
+  0.00605177,  0.03229075, -0.01595549, -0.00646622, -0.01030582, -0.03389338,
+  0.00265833, -0.0066327,  -0.02148847, -0.00350176, -0.04536879,  0.08309361,
+  0.00468482, -0.06002165,  0.10744572, -0.0034748,  -0.05103638,  0.08126458,
+  0.00258044, -0.03308586,  0.05241467,  0.,          0.       ,   0.,
+  0.    ,      0.   ,       0. ,         0.  ,        0.,          0.,
+  0.   ,       0.    ,      0.        ]
+)
     ort_inputs = {'input': np.zeros((1,1170),dtype=np.float32)} 
     for i in range(10):
         t2 = time.time()
