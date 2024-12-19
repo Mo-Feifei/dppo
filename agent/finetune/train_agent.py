afce3ab7b9f51f537e532260d7b2269e0803b96e
qxy <qxy@qxy-Ubuntu20.04>
2024-12-19 11:48:29 +0800

    add finetune

--- agent/finetune/train_agent.py
+++ agent/finetune/train_agent.py
@@ -56,7 +56,7 @@ class TrainAgent:
             action_dim=cfg.action_dim,
             **cfg.env.specific if "specific" in cfg.env else {},
         )
-        if not env_type == "furniture":
+        if not env_type == "furniture" and not env_type == "aliengo":
             self.venv.seed(
                 [self.seed + i for i in range(cfg.env.n_envs)]
             )  # otherwise parallel envs might have the same initial states!
